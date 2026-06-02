09050f9e421c2c85d5f64c7f30e9785927858abe
Wzzz-10086 <100516212+Wzzz-10086@users.noreply.github.com>
2026-06-02 18:55:34 +0800

    chore: update auto-pr upstream default

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Wzzz-10086/scoopApps:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
